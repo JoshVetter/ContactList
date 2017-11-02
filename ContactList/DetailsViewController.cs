26b46bfa0803e089ac8a4e4a8dcb11da4c21a89f
JoshVetter <32143305+JoshVetter@users.noreply.github.com>
2017-11-02 13:27:41 -0600

    it's coming along

---
+++ ContactList/DetailsViewController.cs
@@ -0,0 +1,14 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace ContactList
+{
+    public partial class DetailsViewController : UIView
+    {
+        public DetailsViewController (IntPtr handle) : base (handle)
+        {
+            
+        }
+    }
+}
---
+++ ContactList/labelContactName.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace ContactList
+{
+    public partial class labelContactName : UILabel
+    {
+        public labelContactName (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
